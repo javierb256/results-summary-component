a034a1495cb3c3cc536fe0fd54fa0eb5026d55e0
= <44247277+javierb256@users.noreply.github.com>
2023-08-23 16:34:39 -0700

    finished first desktop layout

---
+++ script.js
@@ -0,0 +1,20 @@
+let details;
+const colors = ['reaction', 'memory', 'verbal', 'visual']
+fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        let temp = document.querySelector("#summary-breakdown");
+        let skills = "";
+        let index = 0;
+        for(skill of data) {
+            skills += `
+                <div class="breakdown ${colors[index]}">
+                <p class="image-title"><img src="${skill.icon}"/>${skill.category}</p>
+                <p class="your-score">${skill.score} <span class="total-score">/ 100</span></p>
+                </div>
+            `
+            index++;
+        }
+        temp.innerHTML = skills
+    })
+
